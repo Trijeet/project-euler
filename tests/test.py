f531b2abbcd24023abc637c87ba54a1c3a160fe8
Trijeet Ganguly <trijeet.ganguly@gmail.com>
2020-10-26 06:40:11 +0530

    Add tests; fix bug in 2; update gitignore

---
+++ tests/test.py
@@ -0,0 +1,43 @@
+import os
+import json
+from colorama import Fore, Back, Style
+
+with open('solutions.json') as solutions:
+    data = json.load(solutions)
+    total_tests = len(data['problems'])
+    correct_tests = 0
+    failed_tests = 0
+    for problem in data['problems']:
+        filename = problem['filename']
+        answer = str(problem['solution'])
+
+        #Execute file and get answer
+        exec_file = filename + ".exe"
+        cmd = "g++ ../" + filename + " -o " + exec_file
+        result = os.system(cmd)
+        if (result):
+            print("Error caught at running file " + filename)
+            print(result)
+            result = "NaN"
+        else:
+            cmd = "./" + exec_file
+            os_file = os.popen(cmd)
+            result = os_file.read()
+            os_file.close()
+        #Compare solutions
+        if (answer.strip() == result.strip()):
+            correct_tests += 1
+            print(Fore.BLACK + Back.GREEN + "Test case passed for " + filename, end = "")
+            print(Style.RESET_ALL)
+        else:
+            failed_tests += 1
+            print(Back.RED + "Test case failed for " + filename, end = "")
+            print(Style.RESET_ALL)
+    print("Stats:")
+    print(str(correct_tests) + " passed out of " + str(total_tests))
+    #Removing all exec files
+    cmd = "rm -rf *.exe"
+    result = os.system(cmd)
+    if (result):
+        print("Error in deleting files")
+        print(result)
