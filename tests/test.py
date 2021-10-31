5379c28fddbd3cf5577c8ef1341cbe5bca59c1ba
Trijeet Ganguly <trijeet.ganguly@gmail.com>
2021-11-01 01:01:51 +0530

    Refactor test script (#13)

--- tests/test.py
+++ tests/test.py
@@ -3,47 +3,35 @@ import sys
 import json
 from colorama import Fore, Back, Style
 
-failed_tests = -1
 with open('solutions.json') as solutions:
     data = json.load(solutions)
     total_tests = len(data)
     correct_tests = 0
     failed_tests = 0
-    for key in data:
-        filename = key
-        answer = data[key]
-
+    for filename, answer in data.items():
         #Execute file and get answer
         exec_file = filename + ".exe"
         cmd = "g++ -w ../solutions/" + filename + " -o " + exec_file
-        result = os.system(cmd)
-        if result:
-            print("Error caught at running file " + filename)
-            print(result)
-            result = "NaN"
-        else:
+        if os.system(cmd) != "":
             cmd = "./" + exec_file
             os_file = os.popen(cmd)
             result = os_file.read()
             os_file.close()
+
         #Compare solutions
         if answer.strip() == result.strip():
             correct_tests += 1
-            print(Fore.BLACK + Back.GREEN + "Test case passed for " + filename, end = "")
-            print(Style.RESET_ALL)
+            print(Fore.BLACK + Back.GREEN + "Test case passed for " + filename + Style.RESET_ALL)
         else:
             failed_tests += 1
-            print(Back.RED + "Test case failed for " + filename, end = "")
-            print(Style.RESET_ALL)
-    print("Stats:")
-    print(str(correct_tests) + " passed out of " + str(total_tests))
+            print(Back.RED + "Test case failed for " + filename + Style.RESET_ALL)
+
+    print("Stats:\n" + str(correct_tests) + " passed out of " + str(total_tests))
+
     #Removing all exec files
     cmd = "rm -rf *.exe"
     result = os.system(cmd)
     if result:
-        print("Error in deleting files")
-        print(result)
-if failed_tests != 0:
-    sys.exit(1)
-else:
-    sys.exit(0)
+        print("Error in deleting files " + result)
+
+    sys.exit(failed_tests)
